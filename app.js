3ea940c7451399c7a2de0dd4c3ec748f2e88c913
akeshari986 <22bcs055@iiitdmj.ac.in>
2025-03-05 15:45:42 +0530

    created auth app gambit

---
+++ app.js
@@ -0,0 +1,39 @@
+import express from 'express';
+import { config } from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { connection } from './database/dbConnection.js';
+import { errorMiddleware } from './middlewares/error.js';
+import userRouter from './routes/userRouter.js';
+import { removeUnverifiedAccounts } from './automation/removeUnverifiedAccounts.js';
+
+export const app = express();
+config({ path: './config.env' });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/v1/user', userRouter);
+
+removeUnverifiedAccounts();
+connection();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+app.use(express.static(path.resolve(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
+});
+
+app.use(errorMiddleware);
---
+++ database/dbConnection.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { config } from "dotenv";
+
+// Load environment variables from .env file
+config();
+
+export const connection = () => {
+  mongoose
+    .connect(`mongodb+srv://ayushk986:${encodeURIComponent('ayush_123')}@expense-tracker.rqbeoe2.mongodb.net/?retryWrites=true&w=majority&appName=Expense-tracker`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: "MERN_AUTHENTICATION",
+    })
+    .then(() => {
+      console.log("Connected to MongoDB Atlas.");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB Atlas:", err);
+    });
+};
